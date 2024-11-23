9769cd7b6f14e04a5e909e1fef63581c3e8961a0
Pablo Pincay Alvarez <72560306+Savecoders@users.noreply.github.com>
2024-11-23 00:09:38 +0000

    test: apply unit test in registers

--- App/Test/TestCompra.cs
+++ App/Test/TestCompra.cs
@@ -10,7 +10,31 @@ namespace Test
         [TestMethod]
         public void RegistrarCompra()
         {
-            
+            Compra compra = new Compra();
+
+            // usuario compra
+            compra.oUsuario = new Usuario();
+            compra.oUsuario.IdUsuario = 1;
+
+            compra.oProveedor = new Proveedor();
+            compra.oProveedor.IdProveedor = 1;
+
+            compra.oTransportista = new Transportista();
+            compra.oTransportista.IdTransportista = 1;
+
+            compra.TipoDocumento = "boleta";
+            compra.NumeroDocumento = "00001";
+            compra.MontoTotal = 120.00m;
+            compra.FechaCompra = "2024-11-22 12:00:00";
+
+            // Detalle de compra
+            //compra.oDetalle_Compra = new List<Detalle_Compra>();
+            //Detalle_Compra detalleCompra = new Detalle_Compra();
+
+            CapaDatos datos = new CapaDatos();
+            string mensaje = "";
+            Assert.IsTrue(datos.registrarCompra(compra, null, out mensaje));
+
         }
     }
 }
--- App/Test/TestVenta.cs
+++ App/Test/TestVenta.cs
@@ -9,7 +9,30 @@ namespace Test
         [TestMethod]
         public void RegistrarVenta()
         {
+            Venta venta = new Venta();
+
+            // usuario venta
+            venta.oUsuario = new Usuario();
+            venta.oUsuario.IdUsuario = 1;
+
+            venta.TipoDocumento = "01";
+            venta.NumeroDocumento = "123456789";
+            venta.DocumentoCliente = "123456789";
+            venta.NombreCliente = "Roberto Perez";
+            venta.MontoPago = 100;
+            venta.MontoCambio = 100;
+            venta.MontoTotal = 100;
+            venta.Descuento = 100;
+            venta.FechaVenta = "2024-11-22";
+
+            // Detalle de venta
+            //venta.oDetalle_Venta = new List<Detalle_Venta>();
+            //Detalle_Venta detalleVenta = new Detalle_Venta();
+
+            CapaDatos datos = new CapaDatos();
+            string mensaje = "";
+            Assert.IsTrue(datos.registrarVenta(venta, null, out mensaje));
 
         }
-    } 
+    }
 }
