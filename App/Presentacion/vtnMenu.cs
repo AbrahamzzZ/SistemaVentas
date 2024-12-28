907690daf1baf39a57863d9d4a32fce392979ce9
AbrahamzzZ <hermanosfarfan@gmail.com>
2024-12-28 00:56:37 -0500

    Se cambio el nombre de algunas variables

--- App/Presentacion/vtnMenu.cs
+++ App/Presentacion/vtnMenu.cs
@@ -13,22 +13,22 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Presentacion
 {
-    public partial class vtnMenu : Form
+    public partial class VtnMenu : Form
     {
         private Usuario nombreUsuarioActual;
         private static Form formularioActivo = null;
-        public vtnMenu(Usuario objusuario)
+        public VtnMenu(Usuario objusuario)
         {
             nombreUsuarioActual = objusuario;
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
         }
 
-        private void vtnMenu_Load(object sender, EventArgs e)
+        private void VtnMenu_Load(object sender, EventArgs e)
         {
             List<Permiso> listaVerificar = new CN_Permiso().Permisos(nombreUsuarioActual.IdUsuario);
 
-            foreach (ToolStripMenuItem iconMenu in menu2.Items)
+            foreach (ToolStripMenuItem iconMenu in MenuPrincipal.Items)
             {
                 bool encontrado = listaVerificar.Any(m => m.NombreMenu == iconMenu.Name);
                 if (encontrado == false)
@@ -36,11 +36,11 @@ namespace Presentacion
                     iconMenu.Visible = false;
                 }
             }
-            lbl2.Text = nombreUsuarioActual.NombreCompleto;
+            LblUsuario.Text = nombreUsuarioActual.NombreCompleto;
             FechaHora.Enabled = true;
         }
 
-        private void verPanel(Form formulario)
+        private void VerPanel(Form formulario)
         {
             if (formularioActivo != null)
             {
@@ -54,119 +54,119 @@ namespace Presentacion
             formulario.Show();
         }
 
-        private void menuItemVerUsuarios_Click_1(object sender, EventArgs e)
+        private void MenuItemVerUsuarios_Click_1(object sender, EventArgs e)
         {
-            verPanel(new VtnUsuarios());
+            VerPanel(new VtnUsuarios());
         }
 
-        private void menuItemInventario_Click_1(object sender, EventArgs e)
+        private void MenuItemInventario_Click_1(object sender, EventArgs e)
         {
-            verPanel(new VtnInventario());
+            VerPanel(new VtnInventario());
         }
 
-        private void menuItemCategoría_Click(object sender, EventArgs e)
+        private void MenuItemCategoría_Click(object sender, EventArgs e)
         {
-            verPanel(new VtnCategoria());
+            VerPanel(new VtnCategoria());
         }
 
-        private void menuItemProducto_Click_1(object sender, EventArgs e)
+        private void MenuItemProducto_Click_1(object sender, EventArgs e)
         {
-            verPanel(new VtnProducto());
+            VerPanel(new VtnProducto());
         }
-        private void menuItemUnidadesMedida_Click(object sender, EventArgs e)
+        private void MenuItemUnidadesMedida_Click(object sender, EventArgs e)
         {
-            verPanel(new VtnUnidadMedida());
-        }
-
-        private void menuItemRealizarVenta_Click(object sender, EventArgs e)
-        {
-            verPanel(new VtnVenta(nombreUsuarioActual));
-        }
-        private void menuItemVerDetallesVenta_Click(object sender, EventArgs e)
-        {
-            verPanel(new vtnVerDetalleVentas());
-        }
-        private void menuItemRealizarCompra_Click(object sender, EventArgs e)
-        {
-            verPanel(new vtnCompra(nombreUsuarioActual));
+            VerPanel(new VtnUnidadMedida());
         }
 
-        private void menuItemVerDetallesCompra_Click(object sender, EventArgs e)
+        private void MenuItemRealizarVenta_Click(object sender, EventArgs e)
         {
-            verPanel(new vtnVerDetalleCompras());
+            VerPanel(new VtnVenta(nombreUsuarioActual));
         }
-        private void menuItemVerProveedores_Click(object sender, EventArgs e)
+        private void MenuItemVerDetallesVenta_Click(object sender, EventArgs e)
         {
-            verPanel(new VtnProveedor());
+            VerPanel(new vtnVerDetalleVentas());
         }
-
-        private void menuItemVerTransportistas_Click(object sender, EventArgs e)
+        private void MenuItemRealizarCompra_Click(object sender, EventArgs e)
         {
-            verPanel(new VtnTransportista());
-        }
-        private void menuItemVerSucursales_Click(object sender, EventArgs e)
-        {
-            verPanel(new VtnSucursal());
-        }
-        private void menuItemVerClientes_Click(object sender, EventArgs e)
-        {
-            verPanel(new VtnCliente());
-        }
-        private void menuItemReportesCompras_Click(object sender, EventArgs e)
-        {
-            verPanel(new vtnReportesCompras());
+            VerPanel(new vtnCompra(nombreUsuarioActual));
         }
 
-        private void menuItemGraficasCompras_Click(object sender, EventArgs e)
+        private void MenuItemVerDetallesCompra_Click(object sender, EventArgs e)
         {
-            verPanel(new vtnGraficaCompras());
+            VerPanel(new vtnVerDetalleCompras());
+        }
+        private void MenuItemVerProveedores_Click(object sender, EventArgs e)
+        {
+            VerPanel(new VtnProveedor());
         }
 
-        private void menuItemReportesVentas_Click(object sender, EventArgs e)
+        private void MenuItemVerTransportistas_Click(object sender, EventArgs e)
         {
-            verPanel(new vtnReportesVentas());
+            VerPanel(new VtnTransportista());
+        }
+        private void MenuItemVerSucursales_Click(object sender, EventArgs e)
+        {
+            VerPanel(new VtnSucursal());
+        }
+        private void MenuItemVerClientes_Click(object sender, EventArgs e)
+        {
+            VerPanel(new VtnCliente());
+        }
+        private void MenuItemReportesCompras_Click(object sender, EventArgs e)
+        {
+            VerPanel(new vtnReportesCompras());
         }
 
-        private void menuItemGraficasVentas_Click(object sender, EventArgs e)
+        private void MenuItemGraficasCompras_Click(object sender, EventArgs e)
         {
-            verPanel(new vtnGraficaVentas());
-        }
-        private void menuItemVerOfertas_Click(object sender, EventArgs e)
-        {
-            verPanel(new vtnOferta());
+            VerPanel(new vtnGraficaCompras());
         }
 
-        private void menuItemVerReclamos_Click(object sender, EventArgs e)
+        private void MenuItemReportesVentas_Click(object sender, EventArgs e)
         {
-            //verPanel(new VtnReclamo());
-        }
-        private void menuItemVerNegocio_Click(object sender, EventArgs e)
-        {
-            verPanel(new vtnNegocio());
+            VerPanel(new vtnReportesVentas());
         }
 
-        private void menuItemVerAutor_Click(object sender, EventArgs e)
+        private void MenuItemGraficasVentas_Click(object sender, EventArgs e)
         {
-            verPanel(new VtnAutor());
+            VerPanel(new vtnGraficaVentas());
+        }
+        private void MenuItemVerOfertas_Click(object sender, EventArgs e)
+        {
+            VerPanel(new vtnOferta());
         }
 
-        private void menuItemVerPrograma_Click(object sender, EventArgs e)
+        private void MenuItemVerReclamos_Click(object sender, EventArgs e)
         {
-            verPanel(new VtnPrograma());
+            //VerPanel(new VtnReclamo());
+        }
+        private void MenuItemVerNegocio_Click(object sender, EventArgs e)
+        {
+            VerPanel(new vtnNegocio());
         }
 
-        private void btnRegresar_Click(object sender, EventArgs e)
+        private void MenuItemVerAutor_Click(object sender, EventArgs e)
+        {
+            VerPanel(new VtnAutor());
+        }
+
+        private void MenuItemVerPrograma_Click(object sender, EventArgs e)
+        {
+            VerPanel(new VtnPrograma());
+        }
+
+        private void FechaHora_Tick(object sender, EventArgs e)
+        {
+            LblHora.Text = DateTime.Now.ToString("hh:mm:ss");
+            LblFecha.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void BtnRegresar_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("¿Desea salir del sistema?", "Menú principal", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
                 this.Close();
             }
-        }
-
-        private void FechaHora_Tick(object sender, EventArgs e)
-        {
-            lblHora.Text = DateTime.Now.ToString("hh:mm:ss");
-            lblFecha.Text = DateTime.Now.ToLongDateString();
         }
     }
 }
