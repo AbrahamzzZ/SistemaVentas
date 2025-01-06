77f87751341113f6a1ecd2b54e72f057d594fc03
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-01-05 22:35:43 -0500

    Se corrigio la clase Usuario para incluir el patron de diseño Singleton

---
+++ App/Entidad/Usuario_Catched.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Usuario_Catched : Usuario
+    {
+        private Usuario_Catched() { }
+
+        private static Usuario_Catched _intance;
+
+        public static Usuario_Catched GetInstance()
+        {
+            if (_intance == null)
+            {
+                _intance = new Usuario_Catched();
+            }
+            return _intance;
+        }
+    }
+}
