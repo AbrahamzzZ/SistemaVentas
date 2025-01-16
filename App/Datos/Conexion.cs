970270bce5e740c902225d382c8d0a84ea8a82a9
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-01-15 20:43:40 -0500

    Se incorporo la cadena de conexion manual para los test

--- App/Datos/Conexion.cs
+++ App/Datos/Conexion.cs
@@ -13,21 +13,27 @@ namespace Datos
 {
     public class Conexion
     {
-        /// <summary>
-        ///  Método para la conexión de la base de datos.
-        /// </summary>
+
+        /// <summary> 
+        /// Método para la conexión de la base de datos. 
+        /// </summary> 
+        /// <returns>Retorna una conexión SQL abierta.</returns>
         public SqlConnection ConexionBD()
         {
             SqlConnection ConexionBaseDatos = null;
             try
             {
-                //Cadena de recuerdo :( 
-                /*string connectionString = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+                // Usar esta cadena de conexión para ocultarla dentro del código
+                // Obtiene la cadena de conexión desde el archivo de configuración 
+                // Inicializa la conexión con la cadena de conexión obtenida 
+                /*string connectionString = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString; 
                 ConexionBaseDatos = new SqlConnection(connectionString);
                 ConexionBaseDatos.Open();*/
-                ConexionBaseDatos = new SqlConnection("server= DESKTOP-8CQM9OG\\SQLSEXPRESS ; database= Sistema_Ventas ; integrated security=true;");
-                ConexionBaseDatos.Open();
 
+                // Usar esta cadena de conexión para realizar los Test 
+                // Inicializa la conexión con la cadena de conexión proporcionada
+                ConexionBaseDatos = new SqlConnection("server=DESKTOP-8CQM9OG\\SQLSEXPRESS; database=Sistema_Ventas; integrated security=true;");
+                ConexionBaseDatos.Open();
             }
             catch (Exception con)
             {
