b401d5836b2f49ebf58e14f84b5debf179db44b1
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-02-16 20:10:08 -0500

    Se esta aplicando la logica para encriptar la claves-Fase de prueba

---
+++ App/Negocios/Seguridad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class Seguridad
+    {
+        public static string GenerarSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using(var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashClave(string clave, string salt)
+        {
+            using(SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(clave + salt);
+                byte[] hash = sha256.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
